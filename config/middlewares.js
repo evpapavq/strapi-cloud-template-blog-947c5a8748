f7ae6b53e861d79d427c78992a1ebef946706b45
evpapavq <Evangelos.Pappas@avaloq.com>
2024-09-24 16:59:15 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+   {
+    name: 'strapi::cors',
+    config: {
+      enabled:true,
+      origin: ['http://localhost:4200',"*"],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
